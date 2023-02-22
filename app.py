b18aace0d5d94472088980fa9f2a77028242f5bb
pinxunchen <101627915+pinxunchen@users.noreply.github.com>
2023-02-22 20:55:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,62 @@
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from flask import Flask, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, FlexSendMessage
 
 app = Flask(__name__)
 
+# 設定 Channel Access Token 和 Channel Secret
 line_bot_api = LineBotApi('mlQ7oqRMEbtzdaO0lG6BmHe2TxMyNv/nEn75lwpOZE0HR3W+nMB8PjBbrhlqOO5Ic7nie1aVaZZAjbDL4MJsz2jo+cMuPs2v/up2vmoIqv7RBxEx8VR9456FmZqjNc5k5I5j/Cwn3OzbLS5CT+4/BAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('fa1fd1143b0de6b63018eda97d4dcbea')
 
+# 定義處理用戶訊息的函數
+def handle_message(event):
+    # 如果接收到的訊息是「我要預約」，則回覆一個 FlexMessage，包含訂車網址的超連結
+    if event.message.text == '我要預約':
+        flex_message = FlexSendMessage(
+            alt_text='訂車網址',
+            contents={
+                'type': 'bubble',
+                'body': {
+                    'type': 'box',
+                    'layout': 'vertical',
+                    'spacing': 'md',
+                    'contents': [
+                        {
+                            'type': 'text',
+                            'text': '我要訂車',
+                            'size': 'md',
+                            'wrap': True
+                        },
+                        {
+                            'type': 'button',
+                            'style': 'primary',
+                            'action': {
+                                'type': 'uri',
+                                'label': '訂車網址',
+                                'uri': 'https://docs.google.com/forms/d/e/1FAIpQLSd_ll8O23_KXjSfT0CagFJc56iL_6HvTnrdbFBYLCnZ9CFqxQ/viewform'
+                            }
+                        }
+                    ]
+                }
+            }
+        )
+        line_bot_api.reply_message(event.reply_token, flex_message)
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
+# 設定 Webhook 路由，接收 Line 平台發送的事件
+@app.route('/callback', methods=['POST'])
+def webhook():
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
     try:
         handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
-
+    except:
+        return 'Error'
     return 'OK'
 
-
+# 設定處理用戶訊息的路由
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+def handle_message_event(event):
+    handle_message(event)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
