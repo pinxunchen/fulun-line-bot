ee64e84277af07a0a148b9c03d30fb2ff50b2be7
pinxunchen <101627915+pinxunchen@users.noreply.github.com>
2023-02-22 22:53:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,58 +1,65 @@
-from linebot.models import (
-    FlexSendMessage, BubbleContainer, BoxComponent, TextComponent,
-    ButtonComponent, SeparatorComponent, URIAction
-)
 from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, FlexSendMessage
-import json
 
+app = Flask(__name__)
 
+# 設定 Channel Access Token 和 Channel Secret
+
+line_bot_api = LineBotApi('mlQ7oqRMEbtzdaO0lG6BmHe2TxMyNv/nEn75lwpOZE0HR3W+nMB8PjBbrhlqOO5Ic7nie1aVaZZAjbDL4MJsz2jo+cMuPs2v/up2vmoIqv7RBxEx8VR9456FmZqjNc5k5I5j/Cwn3OzbLS5CT+4/BAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('fa1fd1143b0de6b63018eda97d4dcbea')
+
+# 定義處理用戶訊息的函數
 def handle_message(event):
-    message = event.message.text
-    if message == '我要預約':
-        # 建立BubbleContainer
-        bubble = BubbleContainer(
-            size='kilo',
-            header=BoxComponent(
-                layout='vertical',
-                contents=[
-                    TextComponent(text='請選擇您的區域', size='xl', weight='bold')
-                ]
-            ),
-            body=BoxComponent(
-                layout='vertical',
-                contents=[
-                    ButtonComponent(
-                        action=URIAction(uri='https://docs.google.com/forms/d/e/1FAIpQLSd_ll8O23_KXjSfT0CagFJc56iL_6HvTnrdbFBYLCnZ9CFqxQ/viewform',
-                                         label='台北長照'),
-                        height='sm',
-                        offsetBottom='sm'
-                    ),
-                    SeparatorComponent(margin='xs'),
-                    ButtonComponent(
-                        action=URIAction(uri='http://linecorp.com/', label='新北長照'),
-                        offsetTop='md'
-                    ),
-                    SeparatorComponent(margin='md'),
-                    ButtonComponent(
-                        action=URIAction(uri='http://linecorp.com/', label='桃園長照'),
-                        offsetTop='md'
-                    )
-                ],
-                alignItems='center'
-            ),
-            styles={
-                'body': {'separator': True}
+    # 如果接收到的訊息是「我要預約」，則回覆一個 FlexMessage，包含訂車網址的超連結
+    if event.message.text == '我要預約':
+        flex_message = FlexSendMessage(
+            alt_text='訂車網址',
+            contents={
+                'type': 'bubble',
+                'body': {
+                    'type': 'box',
+                    'layout': 'vertical',
+                    'spacing': 'md',
+                    'contents': [
+                        {
+                            'type': 'text',
+                            'text': '請點擊以下連結進行訂車',
+                            'size': 'md',
+                            'wrap': True
+                        },
+                        {
+                            'type': 'button',
+                            'style': 'primary',
+                            'action': {
+                                'type': 'uri',
+                                'label': '台北長照',
+                                'uri': 'https://docs.google.com/forms/d/e/1FAIpQLSd_ll8O23_KXjSfT0CagFJc56iL_6HvTnrdbFBYLCnZ9CFqxQ/viewform'
+                            }
+                        },
+                        {
+                            'type': 'button',
+                            'style': 'primary',
+                            'action': {
+                                'type': 'uri',
+                                'label': '新北長照',
+                                'uri': 'https://docs.google.com/forms/d/e/1FAIpQLSd_ll8O23_KXjSfT0CagFJc56iL_6HvTnrdbFBYLCnZ9CFqxQ/viewform'
+                            }
+                        },
+                        {
+                            'type': 'button',
+                            'style': 'primary',
+                            'action': {
+                                'type': 'uri',
+                                'label': '桃園長照',
+                                'uri': 'https://docs.google.com/forms/d/e/1FAIpQLSd_ll8O23_KXjSfT0CagFJc56iL_6HvTnrdbFBYLCnZ9CFqxQ/viewform'
+                            }
+                        }
+                    ]
+                }
             }
         )
-
-        # 建立FlexSendMessage
-        flex_message = FlexSendMessage(alt_text='請選擇您的區域', contents=bubble)
-
-        # 回覆訊息
         line_bot_api.reply_message(event.reply_token, flex_message)
-
 
 # 設定 Webhook 路由，接收 Line 平台發送的事件
 @app.route('/callback', methods=['POST'])
@@ -65,12 +72,10 @@ def webhook():
         return 'Error'
     return 'OK'
 
-
 # 設定處理用戶訊息的路由
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message_event(event):
     handle_message(event)
-
 
 if __name__ == '__main__':
     app.run()
