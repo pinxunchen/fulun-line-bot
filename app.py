20266b645a25c0b273f1a1e1ea41a2795b2ffb89
pinxunchen <101627915+pinxunchen@users.noreply.github.com>
2023-02-24 16:35:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, FlexSendMessage
-from flex_message import FlexMessage
 
 app = Flask(__name__)
 
@@ -11,100 +10,104 @@ handler = WebhookHandler('fa1fd1143b0de6b63018eda97d4dcbea')
 
 # 定義處理用戶訊息的函數
 def handle_message(event):
-    
     # 如果接收到的訊息是「我要預約」，則回覆一個 FlexMessage，包含訂車網址的超連結
     if event.message.text == '我要預約':
         flex_message = FlexSendMessage(
             alt_text='訂車網址',
             contents={
-               "type": "bubble",
-               "size": "kilo", 
-               "header": {
-               "type": "box",
-               "layout": "vertical",
-               "contents": [
-                {                
-                 "type": "text",
-                "text": "請選擇您的區域",
-                "size": "xl",
-                "weight": "bold",
-                "margin": "none",
-                "offsetStart": "md",
-                "offsetTop": "sm"               
-                }
-                ],
-                "offsetTop": "md",
-                "offsetStart": "xs"
-                },
-                "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                {
-                "type": "text",
-                "text": "台北長照",
-                "size": "lg",
-                "align": "center",
-                "margin": "xl",
-                "action": {
-                "type": "uri",
-                "label": "action",
-                "uri": "http://linecorp.com/"
-                }
-                },
-                {
-                "type": "separator",
-                "margin": "lg"
-                },
-                {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                {
+  "type": "bubble",
+  "size": "kilo",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "請選擇您的區域",
+        "size": "xl",
+        "weight": "bold",
+        "margin": "none",
+        "offsetStart": "md",
+        "offsetTop": "sm"
+      }
+    ],
+    "offsetTop": "md",
+    "offsetStart": "xs"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "台北長照",
+            "size": "lg",
+            "align": "center",
+            "margin": "xl",
+            "action": {
+              "type": "uri",
+              "label": "action",
+              "uri": "http://linecorp.com/"
+            }
+          },
+          {
+            "type": "separator",
+            "margin": "lg"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
                 "type": "text",
                 "text": "新北長照",
                 "size": "lg",
                 "align": "center",
                 "margin": "xl",
                 "action": {
-                "ype": "uri",
-                "label": "action",
-                "uri": "http://linecorp.com/"
+                  "type": "uri",
+                  "label": "action",
+                  "uri": "http://linecorp.com/"
                 }
-                },
-                {
+              },
+              {
                 "type": "separator",
                 "margin": "lg"
-                },
-                {
+              },
+              {
                 "type": "box",
                 "layout": "vertical",
                 "contents": [
-                {
-                "type": "text",
-                "text": "桃園長照",
-                "margin": "xl",
-                "size": "lg",
-                "align": "center",
-                "action": {
-                "type": "uri",
-                "label": "action",
-                "uri": "http://linecorp.com/"
-                }
-                }
+                  {
+                    "type": "text",
+                    "text": "桃園長照",
+                    "margin": "xl",
+                    "size": "lg",
+                    "align": "center",
+                    "action": {
+                      "type": "uri",
+                      "label": "action",
+                      "uri": "http://linecorp.com/"
+                    }
+                  }
                 ],
                 "paddingBottom": "sm"
-                }
-                ]}]}]}})
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
+        )
         line_bot_api.reply_message(event.reply_token, flex_message)
 
-    if event.message.text == 'google':
-        flex_message = FlexSendMessage(alt_text='Google', contents=create_google_flex_message())
-        line_bot_api.reply_message(event.reply_token, flex_message)
+
 
 
 
@@ -128,7 +131,6 @@ def webhook():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message_event(event):
     handle_message(event)
-
 
 if __name__ == '__main__':
     app.run()
