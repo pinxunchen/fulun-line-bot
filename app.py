247e1739a13bac2ce1588cf2251ecdd71b8ab984
pinxunchen <101627915+pinxunchen@users.noreply.github.com>
2023-03-01 15:14:41 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, FlexSendMessage
-from message import *
-
 
 app = Flask(__name__)
 
@@ -10,9 +8,99 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('mlQ7oqRMEbtzdaO0lG6BmHe2TxMyNv/nEn75lwpOZE0HR3W+nMB8PjBbrhlqOO5Ic7nie1aVaZZAjbDL4MJsz2jo+cMuPs2v/up2vmoIqv7RBxEx8VR9456FmZqjNc5k5I5j/Cwn3OzbLS5CT+4/BAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('fa1fd1143b0de6b63018eda97d4dcbea')
 
-
+# 定義處理用戶訊息的函數
 def handle_message(event):
-    message = FlexSendMessage(flex_massage_booking())
+    
+    # 如果接收到的訊息是「我要預約」，則回覆一個 FlexMessage，包含訂車網址的超連結
+    if event.message.text == '我要預約':
+        flex_message = FlexSendMessage(
+            alt_text='訂車網址',
+            contents={
+            "type": "bubble",
+            "size": "kilo",
+            "header": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+            "type": "text",
+            "text": "請選擇您的區域",
+            "size": "xl",
+            "weight": "bold",
+            "margin": "none",
+            "offsetStart": "md",
+            "offsetTop": "sm"
+            }
+            ],
+            "offsetTop": "md",
+            "offsetStart": "xs"
+            },
+            "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+            "type": "text",
+            "text": "台北長照",
+            "size": "lg",
+            "align": "center",
+            "margin": "xl",
+            "action": {
+              "type": "uri",
+              "label": "action",
+              "uri": "http://linecorp.com/"
+            }
+            },
+            {
+            "type": "separator",
+            "margin": "lg"
+            },
+            {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "text": "新北長照",
+                "size": "lg",
+                "align": "center",
+                "margin": "xl",
+                "action": {
+                  "type": "uri",
+                  "label": "action",
+                  "uri": "http://linecorp.com/"
+                }
+              },
+              {
+                "type": "separator",
+                "margin": "lg"
+              },
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "桃園長照",
+                    "margin": "xl",
+                    "size": "lg",
+                    "align": "center",
+                    "action": {
+                      "type": "uri",
+                      "label": "action",
+                      "uri": "http://linecorp.com/"
+                    }
+                  }
+                ],
+                "paddingBottom": "sm"
+                }
+                ]
+                } ]}]} })
+        line_bot_api.reply_message(event.reply_token, flex_message)
 
 
 
@@ -34,10 +122,6 @@ def webhook():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message_event(event):
     handle_message(event)
-    flex_massage_booking()
-    
-
-    
 
 if __name__ == '__main__':
     app.run()
