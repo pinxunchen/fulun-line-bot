acc7c66462192a9a41372d0e45b7b2467fbd263f
pinxunchen <101627915+pinxunchen@users.noreply.github.com>
2023-03-01 21:34:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,126 +1,115 @@
-from flask import Flask, request
+from flask import Flask, request, abort, render_template
+
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, FlexSendMessage
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
 
-app = Flask(__name__)
+from liffpy import LineFrontendFramework as LIFF, ErrorResponse
 
-# 設定 Channel Access Token 和 Channel Secret
-line_bot_api = LineBotApi('mlQ7oqRMEbtzdaO0lG6BmHe2TxMyNv/nEn75lwpOZE0HR3W+nMB8PjBbrhlqOO5Ic7nie1aVaZZAjbDL4MJsz2jo+cMuPs2v/up2vmoIqv7RBxEx8VR9456FmZqjNc5k5I5j/Cwn3OzbLS5CT+4/BAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('fa1fd1143b0de6b63018eda97d4dcbea')
+# ======這裡是呼叫的檔案內容=====
+from message import *
+from new import *
+from Function import *
 
-# 定義處理用戶訊息的函數
-def handle_message(event):
-    # 如果接收到的訊息是「我想預約」，則回覆一個 FlexMessage
-    if event.message.text == '我想預約':
-        flex_message = FlexSendMessage(
-            alt_text='訂車網址',
-            contents={
-            "type": "bubble",
-            "size": "kilo",
-            "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-            "type": "text",
-            "text": "請選擇您的區域",
-            "size": "xl",
-            "weight": "bold",
-            "margin": "none",
-            "offsetStart": "md",
-            "offsetTop": "sm"
-            }
-            ],
-            "offsetTop": "md",
-            "offsetStart": "xs"
-            },
-            "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-            "type": "text",
-            "text": "台北長照",
-            "size": "lg",
-            "align": "center",
-            "margin": "xl",
-            "action": {
-              "type": "uri",
-              "label": "action",
-              "uri": "http://linecorp.com/"
-            }
-            },
-            {
-            "type": "separator",
-            "margin": "lg"
-            },
-            {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": "新北長照",
-                "size": "lg",
-                "align": "center",
-                "margin": "xl",
-                "action": {
-                  "type": "uri",
-                  "label": "action",
-                  "uri": "http://linecorp.com/"
-                }
-              },
-              {
-                "type": "separator",
-                "margin": "lg"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "桃園長照",
-                    "margin": "xl",
-                    "size": "lg",
-                    "align": "center",
-                    "action": {
-                      "type": "uri",
-                      "label": "action",
-                      "uri": "http://linecorp.com/"
-                    }
-                  }
-                ],
-                "paddingBottom": "sm"
-                }
-                ]
-                } ]}]} })
-        line_bot_api.reply_message(event.reply_token, flex_message)
+# ======這裡是呼叫的檔案內容=====
 
-    
-    
+# ======python的函數庫==========
+import os
+import time
+
+# ======python的函數庫==========
 
 
+app = Flask(__name__, template_folder="templates")
+static_tmp_path = os.path.join(os.path.dirname(__file__), "static", "tmp")
 
-# 設定 Webhook 路由，接收 Line 平台發送的事件
-@app.route('/callback', methods=['POST'])
-def webhook():
-    signature = request.headers['X-Line-Signature']
+CHANNEL_ACCESS_TOKEN = "mlQ7oqRMEbtzdaO0lG6BmHe2TxMyNv/nEn75lwpOZE0HR3W+nMB8PjBbrhlqOO5Ic7nie1aVaZZAjbDL4MJsz2jo+cMuPs2v/up2vmoIqv7RBxEx8VR9456FmZqjNc5k5I5j/Cwn3OzbLS5CT+4/BAdB04t89/1O/w1cDnyilFU="
+CHANNEL_SECRET = "fa1fd1143b0de6b63018eda97d4dcbea"
+
+liff_api = LIFF(CHANNEL_ACCESS_TOKEN)
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+try:
+    now_LIFF_APP_number = len(liff_api.get())
+except:
+    now_LIFF_APP_number = 0
+
+target_LIFF_APP_number = 10
+print(target_LIFF_APP_number, now_LIFF_APP_number)
+if now_LIFF_APP_number < target_LIFF_APP_number:
+    for i in range(target_LIFF_APP_number - now_LIFF_APP_number):
+        liff_api.add(view_type="full", view_url="https://www.google.com")
+
+
+@app.route("/")
+def index():
+    return render_template("./liff.html")
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=["POST"])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
     body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
     try:
         handler.handle(body, signature)
-    except:
-        return 'Error'
-    return 'OK'
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
 
-# 設定處理用戶訊息的路由
+
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message_event(event):
-    handle_message(event)
+def handle_message(event):
+    msg = event.message.text
+    if "最新合作廠商" in msg:
+        message = imagemap_message()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif "最新活動訊息" in msg:
+        message = buttons_message()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif "註冊會員" in msg:
+        message = Confirm_Template()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif "旋轉木馬" in msg:
+        message = Carousel_Template()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif "圖片畫廊" in msg:
+        message = test()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif "功能列表" in msg:
+        message = function_list()
+        line_bot_api.reply_message(event.reply_token, message)
 
-if __name__ == '__main__':
-    app.run()
+    else:
+        message = TextSendMessage(text=msg)
+        line_bot_api.reply_message(event.reply_token, message)
+
+
+@handler.add(PostbackEvent)
+def handle_message(event):
+    if event.postback.data == "Maso的鑄鐵坊":
+        message = TextSendMessage(text="Maso的鑄鐵坊，目前施工中...")
+        line_bot_api.reply_message(event.reply_token, message)
+
+
+@handler.add(MemberJoinedEvent)
+def welcome(event):
+    uid = event.joined.members[0].user_id
+    gid = event.source.group_id
+    profile = line_bot_api.get_group_member_profile(gid, uid)
+    name = profile.display_name
+    message = TextSendMessage(text=f"{name}歡迎加入")
+    line_bot_api.reply_message(event.reply_token, message)
+
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
