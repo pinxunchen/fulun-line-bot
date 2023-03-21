44ad79d349f8dd81979af4b7ef5dc32863a1f28e
pinxunchen <101627915+pinxunchen@users.noreply.github.com>
2023-03-21 16:06:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -16,97 +16,6 @@ def handle_message(event):
         flex_message = FlexSendMessage(
             alt_text='訂車網址',
             contents={
-            "type": "bubble",
-            "size": "kilo",
-            "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-            "type": "text",
-            "text": "請選擇您的區域",
-            "size": "xl",
-            "weight": "bold",
-            "margin": "none",
-            "offsetStart": "md",
-            "offsetTop": "sm"
-            }
-            ],
-            "offsetTop": "md",
-            "offsetStart": "xs"
-            },
-            "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-            "type": "text",
-            "text": "台北長照",
-            "size": "lg",
-            "align": "center",
-            "margin": "xl",
-            "action": {
-              "type": "uri",
-              "label": "action",
-              "uri": "http://linecorp.com/"
-            }
-            },
-            {
-            "type": "separator",
-            "margin": "lg"
-            },
-            {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": "新北長照",
-                "size": "lg",
-                "align": "center",
-                "margin": "xl",
-                "action": {
-                  "type": "uri",
-                  "label": "action",
-                  "uri": "http://linecorp.com/"
-                }
-              },
-              {
-                "type": "separator",
-                "margin": "lg"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "桃園長照",
-                    "margin": "xl",
-                    "size": "lg",
-                    "align": "center",
-                    "action": {
-                      "type": "uri",
-                      "label": "action",
-                      "uri": "http://linecorp.com/"
-                    }
-                  }
-                ],
-                "paddingBottom": "sm"
-                }
-                ]
-                } ]}]} })
-        line_bot_api.reply_message(event.reply_token, flex_message)
-
-
-    if event.message.text == '常見問題':
-        flex_message = FlexSendMessage(
-            alt_text='常見問題',
-            contents={
   "type": "carousel",
   "contents": [
     {
@@ -200,11 +109,6 @@ def handle_message(event):
         "spacing": "md",
         "offsetBottom": "sm",
         "paddingAll": "xs"
-      },
-      "styles": {
-        "body": {
-          "separator": true
-        }
       }
     },
     {
