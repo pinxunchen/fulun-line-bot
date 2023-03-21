d3e7bb3bdd7a90183068180d1327a5fd70f5094a
pinxunchen <101627915+pinxunchen@users.noreply.github.com>
2023-03-21 15:15:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -103,12 +103,11 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, flex_message)
 
 
-    elif event.message.text == '常見問題':
+    if event.message.text == '常見問題':
         flex_message = FlexSendMessage(
             alt_text='常見問題',
-            contents={
-  "type": "carousel",
-  "contents": [
+            contents={ "type": "carousel",
+          "contents": [
     {
       "type": "bubble",
       "size": "kilo",
