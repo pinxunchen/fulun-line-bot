925a0419928e263121a3a9018b3c474170d32457
pinxunchen <101627915+pinxunchen@users.noreply.github.com>
2023-05-05 17:45:40 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -113,8 +113,14 @@ def handle_message(event):
         "offsetBottom": "md"
       }
     ]
+  },
+  "styles": {
+    "header": {
+      "backgroundColor": "#FFFFF0"
+    }
   }
-})
+}
+)
         line_bot_api.reply_message(event.reply_token, flex_message)
 
 
